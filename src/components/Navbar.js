f12a52b2a3a99270fdb23373fb700d958d496cb0
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-02 14:55:59 +0100

    Write navbar html

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,11 @@
+import logo from "../assets/images/react-icon-small.png";
+
 export function Navbar() {
-  return <h1>This a navbar bruh</h1>;
+  return (
+    <nav>
+      <img alt="logo" src={logo} className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
